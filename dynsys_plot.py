fd8757ce093b1a4b4b6ec39609772c57757b1e62
Matthew Bonanni <MatthewBonanni@users.noreply.github.com>
2019-02-17 00:29:24 -0500

    Added python plotting function

---
+++ dynsys_plot.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+file = "dynsys_data.csv"
+data = pd.read_csv(file)
+
+fig = plt.figure()
+plt.plot(data.x, data.y, 'b')
+plt.plot(data.x[0], data.y[0], 'ro')
+plt.plot(data.x[len(data.x)-1], data.y[len(data.y)-1], 'r*', markersize=10)
+plt.title('Dynamical System Solver - Trajectory', fontsize=14)
+plt.xlabel('$x$', fontsize=14)
+plt.ylabel('$y$', fontsize=14)
+fig.savefig("trajectory.png")
+
+plt.show()
