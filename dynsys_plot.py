a2b8cabbb5ca9e108d95556f40c9aa092d36dd02
Matthew Bonanni <MatthewBonanni@users.noreply.github.com>
2019-02-20 00:48:57 -0500

    Added comments

--- dynsys_plot.py
+++ dynsys_plot.py
@@ -2,12 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Use latex interpreter in plot
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
+# Import data
 file = "dynsys_data.csv"
 data = pd.read_csv(file)
 
+# Plot data
 fig = plt.figure()
 plt.plot(data.x, data.y, 'b')
 plt.plot(data.x[0], data.y[0], 'ro')
