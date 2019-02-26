0a95f89281884b8641f1ed510614f9c98d52dd9e
Matthew Bonanni <MatthewBonanni@users.noreply.github.com>
2019-02-26 16:33:32 -0500

    Changed velocity field

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include "dsys.h"
+#include <cmath>
 
-double u(double t, double x, double y){return x - y + 1;}
-double v(double t, double x, double y){return 2*x - y;}
+double u(double t, double x, double y){return exp(y)*cos(x - t);}
+double v(double t, double x, double y){return exp(y)*sin(x - t);}
 
 int main(){
-    double dt = 0.1;
+    double dt = 0.01;
     double x0 = 0;
-    double y0 = 0;
+    double y0 = -0.1;
     double tmax = 10;
 
     dsys sys1 (dt, x0, y0);
     sys1.fill_tvec(tmax);
     sys1.set_vel(*u, *v);
-    sys1.march_ie();
+    sys1.march_ab();
     sys1.printtraj();
     sys1.export_data("data.csv");
 }
